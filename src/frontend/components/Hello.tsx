62c5ac55ec08b210b8de58ef2e723cdf845291c9
Michał Romaszkin <romaszkin13@o2.pl>
2020-08-01 11:36:25 +0200

    Add Sass support

--- src/frontend/components/Hello.tsx
+++ src/frontend/components/Hello.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import '../styles/main.scss';
 
 export function Hello() {
   return <h1>Hello, world!</h1>;
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 // Small trick to have Webpack IntelliSense in VSCode
 /** @type {import('webpack').Configuration} */
 module.exports = {
-  mode: 'none',
+  mode: 'development',
   devtool: 'source-map',
   entry: {
     app: path.resolve(__dirname, 'src', 'frontend', 'index.tsx'),
@@ -31,6 +32,22 @@ module.exports = {
         exclude: '/node_modules/',
       },
       {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              sassOptions: {
+                outputStyle: 'compressed',
+              },
+            },
+          },
+        ],
+      },
+      {
         enforce: 'pre',
         test: /\.js$/,
         loader: 'source-map-loader',
@@ -45,9 +62,21 @@ module.exports = {
       },
     ],
   },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+      },
+    },
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'src', 'frontend', 'index.html'),
     }),
+    new MiniCssExtractPlugin(),
   ],
 };
